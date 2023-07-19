6f94dd3fbd650f153b761d0e3c94181ad79f3595
EvkoN821 <evkonikit612@gmail.com>
2023-07-19 15:17:55 +0300

    env

--- environment.py
+++ environment.py
@@ -1,19 +1,11 @@
 import time
 import mysql.connector
 import math
-from .utils.parser import parse_tpcc, parse_sysbench, parse_oltpbench, parse_cloudbench, ConfigParser
 import threading
-from subprocess import Popen, TimeoutExpired, STDOUT, PIPE
-from multiprocessing import Manager, Value, Process
 import os
 import psutil
-import numpy as np
-from .utils.resource_monitor import ResourceMonitor
-from knobs_definition import value_type_metrics
+from datetime import datetime
 
-
-im_alive = Value('b', False)
-CPU_CORE = 8
 
 class MysqlConnector:
     def __init__(self, host='localhost', user='root', passwd='root', name='sakila'):
@@ -66,12 +58,22 @@ class MySQLEnv:
         self.default_knobs = default_knobs
 
         self.pid = 9999
+        self.pre_combine_log_file_size = 0
+        self.mycnf = os.environ.get('MYCNF')
 
+    def db_is_alive(self):
+        flag = False
+        while True:
+            for proc in psutil.process_iter():
+                if proc.name() == "mysqld.exe":
+                    flag = True
+                    break
+            if flag:
+                break
+            time.sleep(20)
     ###   ###   ###   ###   ###   ###   ###   ###   ###   ###   ###   ###   ###   ###   ###   ###
     def apply_knobs(self, knobs):
-        # self.db_con.connect_db()
-        # res = False
-        # return res
+        self.db_is_alive()
 
         modify_concurrency = False
         if 'innodb_thread_concurrency' in knobs.keys() and knobs['innodb_thread_concurrency'] * (
@@ -80,21 +82,13 @@ class MySQLEnv:
             modify_concurrency = True
             knobs['innodb_thread_concurrency'] = int(self.pre_combine_log_file_size / (200 * 1024.0)) - 2
 
+
         log_size = knobs['innodb_log_file_size'] if 'innodb_log_file_size' in knobs.keys() else 50331648
         log_num = knobs['innodb_log_files_in_group'] if 'innodb_log_files_in_group' in knobs.keys() else 2
         if 'innodb_thread_concurrency' in knobs.keys() and knobs['innodb_thread_concurrency'] * (200 * 1024) > log_num * log_size:
             return False
 
-        cnf_parser = ConfigParser(self.mycnf)
-        konbs_not_in_mycnf = []
-        for key in knobs.keys():
-            if not key in self.knobs_detail.keys():
-                konbs_not_in_mycnf.append(key)
-                continue
-            cnf_parser.set(key, knobs[key])
-        cnf_parser.replace()
-        knobs_rdsL = konbs_not_in_mycnf
-
+        knobs_rdsL = knobs
         try:
             RESTART_WAIT_TIME = 60
             time.sleep(RESTART_WAIT_TIME)
@@ -112,52 +106,47 @@ class MySQLEnv:
             r2 = db_conn.fetch_results(sql2)
             file_num = r2[0]['Value'].strip()
             self.pre_combine_log_file_size = int(file_num) * int(file_size)
+
+            if modify_concurrency:
+                knobs_rdsL['innodb_thread_concurrency'] = true_concurrency
+                knobs['innodb_thread_concurrency'] = true_concurrency
+
             if len(knobs_rdsL):
                 tmp_rds = {}
                 for knob_rds in knobs_rdsL:
                     tmp_rds[knob_rds] = knobs[knob_rds]
-                self.apply_rds_knobs(tmp_rds)
-            if modify_concurrency:
-                tmp = {}
-                tmp['innodb_thread_concurrency'] = true_concurrency
-                self.apply_rds_knobs(tmp)
-                knobs['innodb_thread_concurrency'] = true_concurrency
+
+                db_conn = MysqlConnector(host=self.host,
+                                         port=self.port,
+                                         user=self.user,
+                                         passwd=self.passwd,
+                                         name=self.dbname,
+                                         socket=self.sock)
+                if 'innodb_io_capacity' in tmp_rds.keys():
+                    tmp_rds['innodb_io_capacity'] = 2 * int(tmp_rds['innodb_io_capacity'])
+                for k, v in tmp_rds:
+                    sql = 'SHOW GLOBAL VARIABLES LIKE "{}";'.format(k)
+                    r = db_conn.fetch_results(sql)
+                    if r[0]['Value'] == v:
+                        return True
+                    sql = f"SET GLOBAL {k}={v}"
+                    try:
+                        db_conn.execute(sql)
+                    except:
+                        sql = f"SET {k}={v}"
+                        db_conn.execute(sql)
+                db_conn.close_db()
         except:
             return False
         return True
 
-    def apply_rds_knobs(self, knobs):
-        db_conn = MysqlConnector(host=self.host,
-                                 port=self.port,
-                                 user=self.user,
-                                 passwd=self.passwd,
-                                 name=self.dbname,
-                                 socket=self.sock)
-        if 'innodb_io_capacity' in knobs.keys():
-            self.set_rds_param(db_conn, 'innodb_io_capacity_max', 2 * int(knobs['innodb_io_capacity']))
-        for key in knobs.keys():
-            self.set_rds_param(db_conn, key, knobs[key])
-        db_conn.close_db()
-        return True
-
     ###
 
-    def get_external_metrics(self, filename=''):
-        result = ''
-        if self.workload['name'] == 'tpcc':
-            result = parse_tpcc(filename)
-        elif self.workload['name'] == 'tpcc_rds':
-            result = parse_tpcc(filename)
-        elif self.workload['name'] == 'sysbench':
-            result = parse_sysbench(filename)
-        elif self.workload['name'] == 'sysbench_rds':
-            result = parse_sysbench(filename)
-        elif self.workload['name'] == 'oltpbench':
-            result = parse_oltpbench('results/{}.summary'.format(filename))
-
-        else:
-            result = parse_cloudbench(filename)
-        return result
+    def get_latency(self):
+        t1 = float(datetime.utcnow().strftime('%S.%f'))
+        self.db_con.execute("SELECT COUNT(*) FROM actor")
+        t2 = float(datetime.utcnow().strftime('%S.%f'))
+        return math.fabs(t2-t1)
 
     def get_internal_metrics(self, internal_metrics):
         global BENCHMARK_RUNNING_TIME, BENCHMARK_WARMING_TIME, TIMEOUT, RESTART_FREQUENCY
@@ -175,7 +164,7 @@ class MySQLEnv:
             print(counter)
             timer = threading.Timer(float(_period), collect_metric, (counter,))
             timer.start()
-            if counter >= count or not im_alive.value:
+            if counter >= count:
                 timer.cancel()
             try:
                 db_conn = MysqlConnector(host=self.host,
@@ -188,7 +177,6 @@ class MySQLEnv:
                 if counter > warmup:
                     self.connect_sucess = False
                     return
-
             try:
                 if counter > warmup:
                     sql = 'SELECT NAME, COUNT from information_schema.INNODB_METRICS where status="enabled" ORDER BY NAME'
@@ -197,117 +185,17 @@ class MySQLEnv:
                     for (k, v) in res:
                         res_dict[k] = v
                     internal_metrics.append(res_dict)
-            except Exception as err:
+            except Exception:
                 self.connect_sucess = False
-
 
         collect_metric(_counter)
         return internal_metrics
 
-    def get_states(self, collect_cpu=0):
-        start = time.time()
-        self.connect_sucess = True
-        p = psutil.Process(self.pid)
-        if len(p.cpu_affinity()) != CPU_CORE:
-            command = 'sudo cgclassify -g memory,cpuset:sever ' + str(self.pid)
-            os.system(command)
-
-        internal_metrics = Manager().list()
-        im = Process(target=self.get_internal_metrics, args=(internal_metrics,))
-        im_alive.value = True
-        im.start()
-        if collect_cpu:
-            rm = ResourceMonitor(self.pid, 1, BENCHMARK_WARMING_TIME, BENCHMARK_RUNNING_TIME)
-            rm.run()
-
-
-
-        ####!  get_benchmark_cmd()
-        # cmd, filename = self.get_benchmark_cmd()
-        # v = p.cpu_percent()
-        # print("[{}] benchmark start!".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-        # p_benchmark = Popen(cmd, shell=True, stderr=STDOUT, stdout=PIPE, close_fds=True)
-        # try:
-        #     outs, errs = p_benchmark.communicate(timeout=TIMEOUT)
-        #     ret_code = p_benchmark.poll()
-        #     if ret_code == 0:
-        #         print("[{}] benchmark finished!".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-        # except TimeoutExpired:
-        #     print("[{}] benchmark timeout!".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-        # clear_cmd = """mysqladmin processlist -uroot -S$MYSQL_SOCK | awk '$2 ~ /^[0-9]/ {print "KILL "$2";"}' | mysql -uroot -S$MYSQL_SOCK """
-        # Popen(clear_cmd, shell=True, stderr=STDOUT, stdout=PIPE, close_fds=True)
-        # print("[{}] clear processlist".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-        # im_alive.value = False
-        # im.join()
-        # if collect_cpu:
-        #     rm.terminate()
-        #
-        # if not self.connect_sucess:
-        #
-        #     return None
-        # external_metrics = self.get_external_metrics(filename)
-        # internal_metrics, dirty_pages, hit_ratio, page_data = self._post_handle(internal_metrics)
-        #
-        # if collect_cpu:
-        #     monitor_data_dict = rm.get_monitor_data()
-        #     interval = time.time() - start
-        #     avg_read_io = sum(monitor_data_dict['io_read']) / (len(monitor_data_dict['io_read']) + 1e-9)
-        #     avg_write_io = sum(monitor_data_dict['io_write']) / (len(monitor_data_dict['io_write']) + 1e-9)
-        #     avg_virtual_memory = sum(monitor_data_dict['mem_virtual']) / (len(monitor_data_dict['mem_virtual']) + 1e-9)
-        #     avg_physical_memory = sum(monitor_data_dict['mem_physical']) / (
-        #                 len(monitor_data_dict['mem_physical']) + 1e-9)
-        #     resource = (None, avg_read_io, avg_write_io, avg_virtual_memory, avg_physical_memory, dirty_pages, hit_ratio, page_data)
-        #
-        #     return external_metrics, internal_metrics, resource
-        # else:
-        #     return external_metrics, internal_metrics, [0]*8
-
-
-    def _post_handle(self, metrics):
-        result = np.zeros(65)
-
-        def do(metric_name, metric_values):
-            metric_type = 'counter'
-            if metric_name in value_type_metrics:
-                metric_type = 'value'
-            if metric_type == 'counter':
-                return float(metric_values[-1] - metric_values[0]) * 23 / len(metric_values)
-            else:
-                return float(sum(metric_values)) / len(metric_values)
-
-        keys = list(metrics[0].keys())
-        keys.sort()
-        total_pages = 0
-        dirty_pages = 0
-        request = 0
-        reads = 0
-        page_data = 0
-        page_size = 0
-        page_misc = 0
-        for idx in range(len(keys)):
-            key = keys[idx]
-            data = [x[key] for x in metrics]
-            result[idx] = do(key, data)
-            if key == 'buffer_pool_pages_total':
-                total_pages = result[idx]
-            elif key == 'buffer_pool_pages_dirty':
-                dirty_pages = result[idx]
-            elif key == 'buffer_pool_read_requests':
-                request = result[idx]
-            elif key == 'buffer_pool_reads':
-                reads = result[idx]
-            elif key == 'buffer_pool_pages_data':
-                page_data = result[idx]
-            elif key == 'innodb_page_size':
-                page_size = result[idx]
-            elif key == 'buffer_pool_pages_misc':
-                page_misc = result[idx]
-        dirty_pages_per = dirty_pages / total_pages
-        hit_ratio = request / float(request + reads)
-        page_data = (page_data + page_misc) * page_size / (1024.0 * 1024.0 * 1024.0)
-
-        return result, dirty_pages_per, hit_ratio, page_data
-
+    def get_states(self):
+        #internal_metrics = Manager().list()
+        external_metrics = self.get_latency()
+        internal_metrics = self.get_internal_metrics()
+        return external_metrics, internal_metrics
 
     ###   ###   ###   ###   ###   ###   ###   ###   ###   ###   ###   ###   ###   ###   ###   ###   ###
     def step(self, knobs):
--- knobs_definition.py
+++ knobs_definition.py
@@ -121,7 +121,7 @@ knobs2 = ['binlog_cache_size',#4096-32768-18446744073709547520
 
 knobs_default = {
     'innodb_adaptive_flushing_lwm': 10,  # 0-10-70
-    'innodb_adaptive_hash_index': True,  # OFF-ON-ON
+    # 'innodb_adaptive_hash_index': True,  # OFF-ON-ON
     'innodb_adaptive_max_sleep_delay': 150000,  # 0-150000-1000000
     'innodb_buffer_pool_instances': 8,  # 1-8-64
     'innodb_buffer_pool_size': 134217728,  # 5242880-134217728-2**64-1
@@ -139,11 +139,11 @@ knobs_default = {
     'binlog_stmt_cache_size': 32768,  # 4096-32768-18446744073709547520
     'eq_range_index_dive_limit': 200,  # 0-200-4294967295
     'host_cache_size': 0,  # 0-(-1)-65536
-    'innodb_adaptive_flushing': True,  # OFF-ON-ON
+    # 'innodb_adaptive_flushing': True,  # OFF-ON-ON
     'innodb_autoextend_increment': 64,  # 1-64-1000
-    'innodb_buffer_pool_dump_now': False,  # OFF-OFF-ON
-    'innodb_buffer_pool_load_at_startup': True,  # OFF-ON-ON
-    'innodb_buffer_pool_load_now': False,  # OFF-OFF-ON
+    # 'innodb_buffer_pool_dump_now': False,  # OFF-OFF-ON
+    # 'innodb_buffer_pool_load_at_startup': True,  # OFF-ON-ON
+    # 'innodb_buffer_pool_load_now': False,  # OFF-OFF-ON
     'innodb_change_buffer_max_size': 25,  # 0-25-50
     'innodb_commit_concurrency': 0,  # 0-0-1000
     'innodb_compression_failure_threshold_pct': 5,  # 0-5-100
@@ -168,7 +168,7 @@ knobs_default = {
     'innodb_page_size': 16384,  # (4096-8192-16384-32768-65536)-16384
     'innodb_purge_batch_size': 300,  # 1-300-5000
     'innodb_purge_threads': 41,  # 1-4-32
-    'innodb_random_read_ahead': False,  # OFF-OFF-ON
+    # 'innodb_random_read_ahead': False,  # OFF-OFF-ON
     'innodb_read_ahead_threshold': 56,  # 0-56-64
     'innodb_read_io_threads': 4,  # 1-4-64
     'innodb_replication_delay': 0,  # 0-0-4294967295
@@ -177,7 +177,7 @@ knobs_default = {
     'innodb_spin_wait_delay': 6,  # 0-6-2**64-1
     'innodb_sync_spin_loops': 30,  # 0-30-4294967295
     'innodb_thread_sleep_delay': 10000,  # 0-10000-1000000
-    'innodb_use_native_aio': True,  # OFF-ON-ON
+    # 'innodb_use_native_aio': True,  # OFF-ON-ON
     'innodb_write_io_threads': 4,  # 1-4-64
     'join_buffer_size': 262144,  # 128-262144-4294967168
     'lock_wait_timeout': 31536000,  # 1-31536000-31536000
@@ -213,7 +213,7 @@ knobs_default = {
     'thread_stack': 262144,  # 131072-262144-18446744073709550592
     'transaction_alloc_block_size': 8192,  # 1024-8192-131072
     'transaction_prealloc_size': 8192,  # 1024-8192-131072
-    'innodb_dedicated_server': False,  # OFF-OFF-ON
+    # 'innodb_dedicated_server': False,  # OFF-OFF-ON
     'innodb_doublewrite_batch_size': 0,  # 0-0-256
     'innodb_doublewrite_files': 8,  # 2-8-256
     'innodb_doublewrite_pages': 4,  # 4-4-512
@@ -226,12 +226,12 @@ knobs_default = {
     'rpl_read_size': 8192,  # 8192-8192-4294959104
     'stored_program_definition_cache': 256,  # 256-256-524288
     'tablespace_definition_cache': 256,  # 256-256-524288
-    'temptable_max_ram':  1073741824# 2097152-1073741824-2^64-1
+    'temptable_max_ram':  1073741824 # 2097152-1073741824-2^64-1
 }
 
 knobs_max = {
     'innodb_adaptive_flushing_lwm': 70,  # 0-10-70
-    'innodb_adaptive_hash_index': True,  # OFF-ON-ON
+    # 'innodb_adaptive_hash_index': True,  # OFF-ON-ON
     'innodb_adaptive_max_sleep_delay': 1000000,  # 0-150000-1000000
     'innodb_buffer_pool_instances': 64,  # 1-8-64
     'innodb_buffer_pool_size': (2**64)-1,  # 5242880-134217728-2**64-1
@@ -249,11 +249,11 @@ knobs_max = {
     'binlog_stmt_cache_size': 18446744073709547520,  # 4096-32768-18446744073709547520
     'eq_range_index_dive_limit': 4294967295,  # 0-200-4294967295
     'host_cache_size': 65536,  # 0-(-1)-65536
-    'innodb_adaptive_flushing': True,  # OFF-ON-ON
+    # 'innodb_adaptive_flushing': True,  # OFF-ON-ON
     'innodb_autoextend_increment': 1000,  # 1-64-1000
-    'innodb_buffer_pool_dump_now': True,  # OFF-OFF-ON
-    'innodb_buffer_pool_load_at_startup': True,  # OFF-ON-ON
-    'innodb_buffer_pool_load_now': True,  # OFF-OFF-ON
+    # 'innodb_buffer_pool_dump_now': True,  # OFF-OFF-ON
+    # 'innodb_buffer_pool_load_at_startup': True,  # OFF-ON-ON
+    # 'innodb_buffer_pool_load_now': True,  # OFF-OFF-ON
     'innodb_change_buffer_max_size': 50,  # 0-25-50
     'innodb_commit_concurrency': 1000,  # 0-0-1000
     'innodb_compression_failure_threshold_pct': 100,  # 0-5-100
@@ -278,7 +278,7 @@ knobs_max = {
     'innodb_page_size': 65536,  # (4096-8192-16384-32768-65536)-16384
     'innodb_purge_batch_size': 5000,  # 1-300-5000
     'innodb_purge_threads': 32,  # 1-4-32
-    'innodb_random_read_ahead': True,  # OFF-OFF-ON
+    # 'innodb_random_read_ahead': True,  # OFF-OFF-ON
     'innodb_read_ahead_threshold': 64,  # 0-56-64
     'innodb_read_io_threads': 64,  # 1-4-64
     'innodb_replication_delay': 4294967295,  # 0-0-4294967295
@@ -287,7 +287,7 @@ knobs_max = {
     'innodb_spin_wait_delay': (2**64)-1,  # 0-6-2**64-1
     'innodb_sync_spin_loops': 4294967295,  # 0-30-4294967295
     'innodb_thread_sleep_delay': 1000000,  # 0-10000-1000000
-    'innodb_use_native_aio': True,  # OFF-ON-ON
+    # 'innodb_use_native_aio': True,  # OFF-ON-ON
     'innodb_write_io_threads': 64,  # 1-4-64
     'join_buffer_size': 4294967168,  # 128-262144-4294967168
     'lock_wait_timeout': 31536000,  # 1-31536000-31536000
@@ -323,7 +323,7 @@ knobs_max = {
     'thread_stack': 18446744073709550592,  # 131072-262144-18446744073709550592
     'transaction_alloc_block_size': 131072,  # 1024-8192-131072
     'transaction_prealloc_size': 131072,  # 1024-8192-131072
-    'innodb_dedicated_server': True,  # OFF-OFF-ON
+    # 'innodb_dedicated_server': True,  # OFF-OFF-ON
     'innodb_doublewrite_batch_size': 256,  # 0-0-256
     'innodb_doublewrite_files': 256,  # 2-8-256
     'innodb_doublewrite_pages': 512,  # 4-4-512
@@ -336,12 +336,12 @@ knobs_max = {
     'rpl_read_size': 4294959104,  # 8192-8192-4294959104
     'stored_program_definition_cache': 524288,  # 256-256-524288
     'tablespace_definition_cache': 524288,  # 256-256-524288
-    'temptable_max_ram':  (2**64)-1# 2097152-1073741824-2^64-1
+    'temptable_max_ram':  (2**64)-1 # 2097152-1073741824-2^64-1
 }
 
 knobs_min = {
     'innodb_adaptive_flushing_lwm': 0,  # 0-10-70
-    'innodb_adaptive_hash_index': True,  # OFF-ON-ON
+    # 'innodb_adaptive_hash_index': False,  # OFF-ON-ON
     'innodb_adaptive_max_sleep_delay': 0,  # 0-150000-1000000
     'innodb_buffer_pool_instances': 1,  # 1-8-64
     'innodb_buffer_pool_size': 5242880,  # 5242880-134217728-2**64-1
@@ -359,11 +359,11 @@ knobs_min = {
     'binlog_stmt_cache_size': 4096,  # 4096-32768-18446744073709547520
     'eq_range_index_dive_limit': 0,  # 0-200-4294967295
     'host_cache_size': 0,  # 0-(-1)-65536
-    'innodb_adaptive_flushing': False,  # OFF-ON-ON
+    # 'innodb_adaptive_flushing': False,  # OFF-ON-ON
     'innodb_autoextend_increment': 1,  # 1-64-1000
-    'innodb_buffer_pool_dump_now': False,  # OFF-OFF-ON
-    'innodb_buffer_pool_load_at_startup': False,  # OFF-ON-ON
-    'innodb_buffer_pool_load_now': False,  # OFF-OFF-ON
+    # 'innodb_buffer_pool_dump_now': False,  # OFF-OFF-ON
+    # 'innodb_buffer_pool_load_at_startup': False,  # OFF-ON-ON
+    # 'innodb_buffer_pool_load_now': False,  # OFF-OFF-ON
     'innodb_change_buffer_max_size': 0,  # 0-25-50
     'innodb_commit_concurrency': 0,  # 0-0-1000
     'innodb_compression_failure_threshold_pct': 0,  # 0-5-100
@@ -388,7 +388,7 @@ knobs_min = {
     'innodb_page_size': 4096,  # (4096-8192-16384-32768-65536)-16384
     'innodb_purge_batch_size': 1,  # 1-300-5000
     'innodb_purge_threads': 1,  # 1-4-32
-    'innodb_random_read_ahead': False,  # OFF-OFF-ON
+    # 'innodb_random_read_ahead': False,  # OFF-OFF-ON
     'innodb_read_ahead_threshold': 0,  # 0-56-64
     'innodb_read_io_threads': 1,  # 1-4-64
     'innodb_replication_delay': 0,  # 0-0-4294967295
@@ -397,7 +397,7 @@ knobs_min = {
     'innodb_spin_wait_delay': 0,  # 0-6-2**64-1
     'innodb_sync_spin_loops': 0,  # 0-30-4294967295
     'innodb_thread_sleep_delay': 0,  # 0-10000-1000000
-    'innodb_use_native_aio': False,  # OFF-ON-ON
+    # 'innodb_use_native_aio': False,  # OFF-ON-ON
     'innodb_write_io_threads': 1,  # 1-4-64
     'join_buffer_size': 128,  # 128-262144-4294967168
     'lock_wait_timeout': 1,  # 1-31536000-31536000
@@ -433,7 +433,7 @@ knobs_min = {
     'thread_stack': 131072,  # 131072-262144-18446744073709550592
     'transaction_alloc_block_size': 1024,  # 1024-8192-131072
     'transaction_prealloc_size': 1024,  # 1024-8192-131072
-    'innodb_dedicated_server': False,  # OFF-OFF-ON
+    # 'innodb_dedicated_server': False,  # OFF-OFF-ON
     'innodb_doublewrite_batch_size': 0,  # 0-0-256
     'innodb_doublewrite_files': 2,  # 2-8-256
     'innodb_doublewrite_pages': 4,  # 4-4-512
@@ -446,7 +446,7 @@ knobs_min = {
     'rpl_read_size': 8192,  # 8192-8192-4294959104
     'stored_program_definition_cache': 256,  # 256-256-524288
     'tablespace_definition_cache': 256,  # 256-256-524288
-    'temptable_max_ram':  2097152# 2097152-1073741824-2^64-1
+    'temptable_max_ram':  2097152 # 2097152-1073741824-2^64-1
 }
 
 knobs = [
@@ -545,108 +545,6 @@ knobs = [
     'transaction_alloc_block_size',#1024-8192-131072
     'transaction_prealloc_size',#1024-8192-131072
     'innodb_dedicated_server',#OFF-OFF-ON
-    'innodb_doublewrite_batch_size',#0-0-256
-    'innodb_doublewrite_files',#2-8-256
-    'innodb_doublewrite_pages',#4-4-512
-    'innodb_log_files_in_group',#2-2-100
-    'innodb_log_spin_cpu_abs_lwm',#0-80-4294967295
-    'innodb_log_spin_cpu_pct_hwm',#0-50-100
-    'innodb_log_wait_for_flush_spin_hwm',#0-400-2**64-1
-    'max_relay_log_size',#0-0-1073741824
-    'relay_log_space_limit',#0-0-18446744073709551615
-    'rpl_read_size',#8192-8192-4294959104
-    'stored_program_definition_cache',#256-256-524288
-    'tablespace_definition_cache',#256-256-524288
-    'temptable_max_ram'#2097152-1073741824-2^64-1
-]
-
-value_type_metrics = [
-    'innodb_adaptive_flushing_lwm',#0-10-70
-    'innodb_adaptive_max_sleep_delay',#0-150000-1000000
-    'innodb_buffer_pool_instances',#1-8-64
-    'innodb_buffer_pool_size',#5242880-134217728-2**64-1
-    'innodb_io_capacity',#100-200-2**64-1
-    'innodb_log_file_size',#4194304-50331648-(512GB/innodb_log_files_in_group)
-    'innodb_max_dirty_pages_pct',#0-75-99.99
-    'innodb_max_dirty_pages_pct_lwm',#0-0-99.99
-    'innodb_sync_array_size',#1-1-1024
-    'innodb_thread_concurrency',#0-0-1000
-    'max_heap_table_size',#16384-16777216-18446744073709550592
-    'thread_cache_size',#0-(-1)-16384
-    'tmp_table_size'#1024-16777216-18446744073709551615
-    'binlog_cache_size',#4096-32768-18446744073709547520
-    'binlog_max_flush_queue_time',#0-0-100000
-    'binlog_stmt_cache_size',#4096-32768-18446744073709547520
-    'eq_range_index_dive_limit',#0-200-4294967295
-    'host_cache_size',#0-(-1)-65536
-    'innodb_autoextend_increment',#1-64-1000
-    'innodb_change_buffer_max_size',#0-25-50
-    'innodb_commit_concurrency',#0-0-1000
-    'innodb_compression_failure_threshold_pct',#0-5-100
-    'innodb_compression_level',#0-6-9
-    'innodb_compression_pad_pct_max',#0-50-75
-    'innodb_concurrency_tickets',#1-5000-4294967295
-    'innodb_flush_log_at_timeout',#1-1-2700
-    'innodb_flush_neighbors',#(0-1-2)-1
-    'innodb_flushing_avg_loops',#1-30-1000
-    'innodb_ft_cache_size',#1600000-8000000-80000000
-    'innodb_ft_result_cache_limit',#1000000-2000000000-2**32-1
-    'innodb_ft_sort_pll_degree',#1-2-32
-    'innodb_io_capacity_max',#100-?-2**32-1
-    'innodb_lock_wait_timeout',#1-50-1073741824
-    'innodb_log_buffer_size',#1048576-16777216-4294967295
-    'innodb_lru_scan_depth',#100-1024-2**64-1
-    'innodb_max_purge_lag',#0-0-4294967295
-    'innodb_max_purge_lag_delay',#0-0-10000000
-    'innodb_old_blocks_pct',#5-37-95
-    'innodb_old_blocks_time',#0-1000-2**32-1
-    'innodb_online_alter_log_max_size',#65536-134217728-2**64-1
-    'innodb_page_size',#(4096-8192-16384-32768-65536)-16384
-    'innodb_purge_batch_size',#1-300-5000
-    'innodb_purge_threads',#1-4-32
-    'innodb_read_ahead_threshold',#0-56-64
-    'innodb_read_io_threads',#1-4-64
-    'innodb_replication_delay',#0-0-4294967295
-    'innodb_rollback_segments',#1-128-128
-    'innodb_sort_buffer_size',#65536-1048576-67108864
-    'innodb_spin_wait_delay',#0-6-2**64-1
-    'innodb_sync_spin_loops',#0-30-4294967295
-    'innodb_thread_sleep_delay',#0-10000-1000000
-    'innodb_write_io_threads',#1-4-64
-    'join_buffer_size',#128-262144-4294967168
-    'lock_wait_timeout',#1-31536000-31536000
-    'max_binlog_cache_size',#4096-18446744073709547520-18446744073709547520
-    'max_binlog_size',#4096-1073741824-1073741824
-    'max_binlog_stmt_cache_size',#4096-18446744073709547520-18446744073709547520
-    'max_delayed_threads',#0-20-16384
-    'max_insert_delayed_threads',#0-20-16384
-    'max_join_size',#1-18446744073709551615-18446744073709551615
-    'max_length_for_sort_data',#4-1024-8388608
-    'max_seeks_for_key',#1-4294967295-4294967295
-    'max_sort_length',#4-1024-8388608
-    'max_sp_recursion_depth',#0-0-255
-    'max_write_lock_count',#1-4294967295-4294967295
-    'metadata_locks_cache_size',#1-1024-1048576
-    'optimizer_prune_level',#0-1-1
-    'optimizer_search_depth',#0-62-62
-    'preload_buffer_size',#1024-32768-1073741824
-    'query_alloc_block_size',#1024-8192-4294966272
-    'query_prealloc_size',#8192-8192-18446744073709550592
-    'range_alloc_block_size',#4096-4096-18446744073709550592
-    'read_buffer_size',#8192-131072-2147479552
-    'read_rnd_buffer_size',#1-262144-2147483647
-    'slave_checkpoint_group',#32-512-524280
-    'slave_checkpoint_period',#1-300-4294967295
-    'slave_parallel_workers',#0-4-1024
-    'slave_pending_jobs_size_max',#1024-128M-16EiB
-    'sort_buffer_size',#32768-262144-4294967295
-    'stored_program_cache',#16-256-524288
-    'table_definition_cache',#400-(-1)-524288
-    'table_open_cache',#1-2000-524288
-    'table_open_cache_instances',#1-16-64
-    'thread_stack',#131072-262144-18446744073709550592
-    'transaction_alloc_block_size',#1024-8192-131072
-    'transaction_prealloc_size',#1024-8192-131072
     'innodb_doublewrite_batch_size',#0-0-256
     'innodb_doublewrite_files',#2-8-256
     'innodb_doublewrite_pages',#4-4-512
